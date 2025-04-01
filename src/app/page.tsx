9507e72d6ff5278160574b21c1c99c2f903388f2
OmegazonCMoi <fabroc300@gmail.com>
2025-04-01 23:08:49 +0200

    Oups :/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     return (
         <div className='max-w-5xl mx-auto items-center justify-center min-h-screen flex flex-col mt-[10em]'>
             <h1 className='text-[75px] mb-8' style={{ fontFamily: "Darlington" }}>My Projects</h1>
-            <div id='list' className='w-full grid grid-cols-1 md:grid-cols-3 gap-4'>
+            <div id='list' className='w-full grid grid-cols-1 md:grid-cols-3 px-10 lg:px-0 gap-4'>
                 {sites.map(site => (
                     <Card key={site.id} className=''>
                         <Card.Header>
