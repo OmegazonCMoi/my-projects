4d8c33023996b92af139aa15fc86f096b83fdae8
OmegazonCMoi <fabroc300@gmail.com>
2025-04-01 23:11:08 +0200

    Oups :/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="fr" className='dark'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-950`}
       >
         {children}
       </body>
